2aed9f7fe0f95737b94b48cce08c89f5d485d6eb
Xavier Francisco <xavier.n.francisco@gmail.com>
2020-02-16 21:42:55 +0000

    Apply google-java-format to all files

--- Card.java
+++ Card.java
@@ -1,50 +1,48 @@
-public class Card{
+public class Card {
 
-	char value;
-	char suite;
+  char value;
+  char suite;
 
-	public Card(){
-		this.value = '-';
-		this.suite = '-';
-	}
+  public Card() {
+    this.value = '-';
+    this.suite = '-';
+  }
 
-	public Card(char v, char s){
-		this.value = v;
-		this.suite = s;
-	}
+  public Card(char v, char s) {
+    this.value = v;
+    this.suite = s;
+  }
 
-	public Card(Card c){
-		this.value = c.getValue();
-		this.suite = c.getSuite();
-	}
+  public Card(Card c) {
+    this.value = c.getValue();
+    this.suite = c.getSuite();
+  }
 
-	public String getPlayerCard(){
-		String s = new String();
-		String ss = new String();
-		s = String.valueOf(this.getValue());
-		ss = String.valueOf(this.getSuite());
+  public String getPlayerCard() {
+    String s = new String();
+    String ss = new String();
+    s = String.valueOf(this.getValue());
+    ss = String.valueOf(this.getSuite());
 
-		String sss = new String();
-		sss = s + ss;
-		
-		return sss;
-	}
+    String sss = new String();
+    sss = s + ss;
 
-	public char getValue(){
-		return this.value;
-	}
+    return sss;
+  }
 
-	public char getSuite(){
-		return this.suite;
-	}
+  public char getValue() {
+    return this.value;
+  }
 
-	public void setValue(char v){
-		this.value = v;
-	}
+  public char getSuite() {
+    return this.suite;
+  }
 
-	public void setSuite(char s){
-		this.suite = s;
-	}
+  public void setValue(char v) {
+    this.value = v;
+  }
 
-
+  public void setSuite(char s) {
+    this.suite = s;
+  }
 }
